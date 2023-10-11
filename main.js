81283ec119de617f9833a5a9c8273971267a322c
christinamelnikova <130339715+christinamelnikova@users.noreply.github.com>
2023-10-11 09:03:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 document.addEventListener('mousemove', function (e){
     document.querySelectorAll('.par').forEach(cur=>{
-        // let x = e.clientX / window.innerWidth;
-        // let y = e.clientY / window.innerHeight;
-        // cur.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
-
         let speedX = cur.getAttribute('data-speedX')
         let speedY = cur.getAttribute('data-speedY')
         cur.style.transform = `translate(${e.clientX*speedX/300}px, ${e.clientY*speedY/300}px)`
